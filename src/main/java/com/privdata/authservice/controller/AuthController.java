00014ca044008c2b35e74543314fc82bab853d6e
WVinet <w.vinet.h@gmail.com>
2026-04-01 14:40:12 -0300

    feat: Creacion de Auth controller

---
+++ src/main/java/com/privdata/authservice/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.privdata.authservice.controller;
+
+import com.privdata.authservice.dto.request.LoginRequestDTO;
+import com.privdata.authservice.dto.request.RegisterRequestDTO;
+import com.privdata.authservice.dto.response.LoginResponseDTO;
+import com.privdata.authservice.dto.response.RegisterResponseDTO;
+import com.privdata.authservice.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO request) {
+        RegisterResponseDTO response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
